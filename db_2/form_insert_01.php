3d0d24c62007c18bad4ce6084c2194753839c8d8
がる <m.furusho@gmail.com>
2017-03-07 22:59:21 +0900

    common_function 00 -> 01

---
+++ db_2/form_insert_01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>DB講座中級</title>
+</head>
+
+<body>
+  <form action="./form_insert_fin.php" method="post">
+    名前：<input type="text" name="name" value=""><br>
+    郵便番号(例：999-9999)：<input type="text" name="post" value=""><br>
+    住所：<input type="text" name="address" value=""><br>
+    誕生日：西暦<input type="text" name="birthday_yy" value="">年<input type="text" name="birthday_mm" value="">月<input type="text" name="birthday_dd" value="">日<br>
+    <br>
+    <button type="submit">データ登録</button>
+  </form>
+</body>
+</html>
